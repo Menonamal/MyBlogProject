de5a52e5251cd713e8d8a4e7c78c56393d727ff0
Menonamal <amalupj@gmail.com>
2023-11-09 13:38:53 +0530

    FLIGHT RESERVATION APP

--- flight_reservation_app/src/main/java/com/flight_reservation_app/utilities/EmailUtil.java
+++ flight_reservation_app/src/main/java/com/flight_reservation_app/utilities/EmailUtil.java
@@ -12,25 +12,35 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class EmailUtil {
-	
-	@Autowired
-	private JavaMailSender sender;
-	
-	public void sendItinerary(String toAddress, String filePath) {
-		System.out.println(filePath);
-			
-		MimeMessage message = sender.createMimeMessage();
-		try {
-			MimeMessageHelper messageHelper = new MimeMessageHelper(message,true);
-			messageHelper.setTo(toAddress);
-			messageHelper.setSubject("Itinerary Of Flight");
-			messageHelper.setText("Please find the attached");
-			messageHelper.addAttachment("Itinerary", new File(filePath) );
-			sender.send(message);
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-			
-	}
-
+    
+    @Autowired
+    private JavaMailSender sender;
+    
+    public void sendItinerary(String toAddress, String filePath) {
+        // Basic email address validation
+        if (!isValidEmailAddress(toAddress)) {
+            System.err.println("Invalid recipient email address: " + toAddress);
+            return; // Exit early if the email address is not valid
+        }
+        
+        System.out.println(filePath);
+            
+        MimeMessage message = sender.createMimeMessage();
+        try {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+            messageHelper.setTo(toAddress);
+            messageHelper.setSubject("Itinerary Of Flight");
+            messageHelper.setText("Please find the attached");
+            messageHelper.addAttachment("Itinerary", new File(filePath));
+            sender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    // Validate email address using a simple regex pattern
+    private boolean isValidEmailAddress(String email) {
+        String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        return email.matches(emailRegex);
+    }
 }
